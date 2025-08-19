cb89334b07947cf6d4163afdbd104b2b7fb045f5
Andrea Arcila Cano <andreaac777@gmail.com>
2025-08-18 19:14:32 -0500

    Configuraciones iniciales

---
+++ assets/data/products.ts
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: 'Palito Premium de Puro Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: 2500,
+  },
+  {
+    id: 2,
+    name: 'Palito Premium de Bocadillo y Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: 2500,
+  },
+  {
+    id: 3,
+    name: 'Palito Premium de Jamón y Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: 2500,
+  },
+  {
+    id: 4,
+    name: 'Palito Coctelero de Puro Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+    price: 1000,
+  },
+  {
+    id: 5,
+    name: 'Palito Coctelero de Bocadillo y Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: 1000,
+  },
+  {
+    id: 6,
+    name: 'Palito Coctelero de Jamón y Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+    price: 1000,
+  },
+  {
+    id: 7,
+    name: 'Palito Coctelero de Chocolate y Queso',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+    price: 1000,
+  },
+  {
+    id: 8,
+    name: 'Palipapa',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: 2000,
+  },
+  {
+    id: 9,
+    name: 'Oblea',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: 5000,
+  },
+  {
+    id: 10,
+    name: 'Panocha',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+    price: 6000,
+  },
+];
+
+export default products;
