5d38c42a3c1d511166a7e5be19d4cb62d3368dd0
Andrea Arcila Cano <andreaac777@gmail.com>
2025-08-19 01:33:05 -0500

    Expo Router Screens P2

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
@@ -9,10 +10,10 @@ import { useClientOnlyValue } from '../../components/useClientOnlyValue';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <MaterialCommunityIcons size={25} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
@@ -29,8 +30,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Productos',
+          tabBarIcon: ({ color }) => <TabBarIcon name="food-croissant" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -50,8 +51,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Carrito',
+          tabBarIcon: ({ color }) => <TabBarIcon name="cart-outline" color={color} />,
         }}
       />
     </Tabs>
--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -6,7 +6,7 @@ const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams(); 
   return (
     <View>
-      <Text>ProductDetailsScreen for id: {id}</Text>
+      <Text style={{ fontSize: 20}}>ProductDetailsScreen for id: {id}</Text>
     </View>
   )
 }
